82448c7a3eac3b825c5b150dbc702814dde7797b
Caleb Rutland <drvondevious@Calebs-MBP.attlocal.net>
2021-10-08 20:43:23 -0500

    add REST routes for Tree model

--- app/routes/tree.ts
+++ app/routes/tree.ts
@@ -9,4 +9,37 @@ router.get("/", async (req, res) => {
   res.json(trees);
 });
 
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const tree = await prisma.tree.findUnique({ where: { id } });
+  res.json(tree);
+});
+
+router.post("/", async (req, res) => {
+  const { name, description } = req.body;
+  const tree = await prisma.tree.create({
+    data: {
+      name,
+      description,
+    }
+  });
+  res.json(tree);
+});
+
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name, description } = req.body;
+  const tree = await prisma.tree.update({
+    where: { id },
+    data: { name, description },
+  });
+  res.json(tree);
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const tree = await prisma.tree.delete({ where: { id } });
+  res.json(tree);
+});
+
 export default router;
